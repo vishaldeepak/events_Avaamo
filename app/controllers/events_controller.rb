b21bba773782d2455c553b7b254f152b4927fa4c
vishaldeepak <vishal.deepak@outlook.com>
2021-09-18 10:29:59 +0530

    Added bootstrap for styling

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,4 @@
+class EventsController < ApplicationController
+  def events
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'events', to: 'events#events'
+  # resources :events, only: [] do
+  #   collection do
+  #     get :events
+  #   end
+  # end
 end
